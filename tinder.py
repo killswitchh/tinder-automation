db17be20fbd4b5fa560ed43a7d73108100eacbf2
Balaji J <balaji110499@gmail.com>
2019-07-12 01:00:24 +0530

    OTP Issue fixed
    Code now waits for user to check OTP every 2 seconds and only proceeds if all 6 fields of the OTP text box is filled

--- tinder.py
+++ tinder.py
@@ -7,9 +7,10 @@ from selenium.webdriver.common.by import By
 import time
 
 """
-TODO  :
+TODO
 1 . Open Tinder and login using Phone number[DONE]
-2 . Wait till user enters OTP[NEEDS WORK - Using time.wait() for the meantime]
+2 . Wait till user enters OTP[DONE]
+3 . Ability to like / unlike cards[ERROR - element not found]
 3 . Read Description of cards[TO BE DONE]
 4 . segregate sentences and look for their presence in quote sites from internet[TO BE DONE]
 """
@@ -36,20 +37,27 @@ disableNotificationsx= '//*[@id="content"]/span/div/div[2]/div/div/div[3]/button
 RSwipe='//*[@id="content"]/span/div/div[1]/div/div/main/div/div[1]/div/div[2]/button[3]/span/svg'
 #reject button(left swipe)-
 LSwipe='//*[@id="content"]/span/div/div[1]/div/div/main/div/div[1]/div/div[2]/button[1]/span/svg'
+#information button to view description
+Info='//*[@id="content"]/span/div/div[1]/div/div/main/div/div[1]/div/div[1]/div[3]/div[6]/div[2]/svg'
 
 """
 ---------------------------------------
 """
 
-def login():
+def runSelenium():
+    global url
+    global options
+    global driver
+    global userPhone
     userPhone=input("enter Phone number\n")
     url="https://tinder.com/app/recs"
-
     '''finding chrome driver'''
     options=Options()
     options.binary_location = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
     driver=webdriver.Chrome(options= options ,executable_path=r"C:\Program Files (x86)\Google\chromedriver.exe",)
-
+    
+def login():
+    
     '''opening page'''
     driver.get(url)
     print("url opened")
@@ -67,24 +75,44 @@ def login():
     continuePhone=driver.find_element_by_xpath(phoneCont)
     continuePhone.click()
 
-    '''[NEEDS WORK] Waiting for user to enter OTP'''
-    print("waiting for user to enter otp")
-    #element=WebDriverWait(driver,30)
-    #element.until(EC.presence_of_element_located((By.XPATH,otpCont)))
-    #winddriver.implicitly_wait(30)
-    time.sleep(15)
-    continueOtp=driver.find_element_by_xpath(otpCont)
+    '''Waiting for user to enter OTP'''
+    print("OTP PHASE")
+    classNameChange="Bg($c-gray)" #class name does not have this string when otp is entered
+    while True:
+        time.sleep(2)
+        continueOtp=driver.find_element_by_xpath(otpCont)
+        cname=continueOtp.get_attribute("class")
+        #print(cname)
+        if classNameChange not in cname:
+            print("OTP ENTERED")
+            break
+        else:
+            print("WAITING FOR OTP")
     continueOtp.click()
 
     ''' [NEEDS WORK] Clicking allow location and disable notification button '''
     #driver.implicitly_wait(10)
-    time.sleep(3)
+    time.sleep(2)
     allowLocation = driver.find_element_by_xpath(allowLocationx)
     allowLocation.click()
+    print("Allow Location clicked")
     #driver.implicitly_wait(10)
-    time.sleep(2)
+    time.sleep(1)
     disableNotifications=driver.find_element_by_xpath(disableNotificationsx)
     disableNotifications.click()
+    print("Disable Notifications clicked")
     print("login succesful")
 
+    
+def unlimitedLikes():
+    time.sleep(10)#for the tutorial animation to play out
+    '''Presses the like button once , every second'''
+    while(True):
+        like=driver.find_element_by_xpath(RSwipe)
+        like.click()
+        time.sleep(1)
+
+        
+runSelenium()
 login()
+unlimitedLikes()
