fc295601e2b921aa5a2d2d5f9c0cdec84b37557c
Balaji J <balaji110499@gmail.com>
2019-07-11 17:19:16 +0530

    Comments Added / Code cleaned

--- tinder.py
+++ tinder.py
@@ -7,50 +7,84 @@ from selenium.webdriver.common.by import By
 import time
 
 """
-XPATHS
-
-like button(right swipe) - //*[@id="content"]/span/div/div[1]/div/div/main/div/div[1]/div/div[2]/button[3]/span/svg
-reject button(left swipe)- //*[@id="content"]/span/div/div[1]/div/div/main/div/div[1]/div/div[2]/button[1]/span/svg
-to be identified - '//*[@id="content"]/span/div/div[1]/div/aside/nav/div/div/div/div[1]/div/div[2]'))
-allow location button - '//*[@id="content"]/span/div/div[2]/div/div/div[3]/button[1]'
-enable notifications - not now button - '//*[@id="content"]/span/div/div[2]/div/div/div[3]/button[2]'
+TODO  :
+1 . Open Tinder and login using Phone number[DONE]
+2 . Wait till user enters OTP[NEEDS WORK - Using time.wait() for the meantime]
+3 . Read Description of cards[TO BE DONE]
+4 . segregate sentences and look for their presence in quote sites from internet[TO BE DONE]
 """
 
-userPhone=input("enter Phone number")
-url="https://tinder.com/app/recs"
-#url="https://www.facebook.com/"
-options=Options()
-options.binary_location = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-driver=webdriver.Chrome(options= options ,executable_path=r"C:\Program Files (x86)\Google\chromedriver.exe",)
-driver.get(url)
-print("url opened")
-#driver.maximize_window()
-#print("Window maximized")
 
 
-driver.implicitly_wait(10)
 
-loginp=driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/div/div[3]/div[1]/button')
-loginp.click()
-driver.implicitly_wait(4)
-Phonetextbox=driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/div[2]/div/input').send_keys(userPhone)
-#login=driver.find_element_by_class_name('My.(10px).My(16px)--m')
+"""
+-----------------XPATHS----------------
+"""
+#login button in popup window -
+loginB='//*[@id="modal-manager"]/div/div/div[2]/div/div[3]/div[1]/button'
+#Phonenumber text box -
+EnterPhone='//*[@id="modal-manager"]/div/div/div[2]/div[2]/div/input'
+#OTP continue button -
+otpCont='//*[@id="modal-manager"]/div/div/div[2]/button'
+#Phone Number continue button -
+phoneCont='//*[@id="modal-manager"]/div/div/div[2]/button'
+#Allow Location
+allowLocationx= '//*[@id="content"]/span/div/div[2]/div/div/div[3]/button[1]'
+#Disable Notification
+disableNotificationsx= '//*[@id="content"]/span/div/div[2]/div/div/div[3]/button[2]'
+#like button(right swipe) -
+RSwipe='//*[@id="content"]/span/div/div[1]/div/div/main/div/div[1]/div/div[2]/button[3]/span/svg'
+#reject button(left swipe)-
+LSwipe='//*[@id="content"]/span/div/div[1]/div/div/main/div/div[1]/div/div[2]/button[1]/span/svg'
 
-continueButton=driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/button')
-continueButton.click()
-cont='//*[@id="modal-manager"]/div/div/div[2]/button'
-print("waiting for user to enter otp")
-element=WebDriverWait(driver,30)
-element.until(EC.presence_of_element_located((By.XPATH,cont)))
-#driver.implicitly_wait(30)
-time.sleep(15)
-continue2=driver.find_element_by_xpath(cont)
-continue2.click()
-time.sleep(3)
-allowLocationx = '//*[@id="content"]/span/div/div[2]/div/div/div[3]/button[1]'
-disableNotificationsx = '//*[@id="content"]/span/div/div[2]/div/div/div[3]/button[2]'
-allowLocation = driver.find_element_by_xpath(allowLocationx)
-allowLocation.click()
-time.sleep(2)
-disableNotifications=driver.find_element_by_xpath(disableNotificationsx)
-disableNotifications.click()
+"""
+---------------------------------------
+"""
+
+def login():
+    userPhone=input("enter Phone number\n")
+    url="https://tinder.com/app/recs"
+
+    '''finding chrome driver'''
+    options=Options()
+    options.binary_location = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+    driver=webdriver.Chrome(options= options ,executable_path=r"C:\Program Files (x86)\Google\chromedriver.exe",)
+
+    '''opening page'''
+    driver.get(url)
+    print("url opened")
+    #driver.maximize_window()
+    #print("Window maximized")
+
+    '''waiting for login page to popup'''
+    driver.implicitly_wait(10) #<-not working as intended 
+    loginp=driver.find_element_by_xpath(loginB)
+    loginp.click()
+    driver.implicitly_wait(4)
+
+    '''Entering phone number '''
+    Phonetextbox=driver.find_element_by_xpath(EnterPhone).send_keys(userPhone)
+    continuePhone=driver.find_element_by_xpath(phoneCont)
+    continuePhone.click()
+
+    '''[NEEDS WORK] Waiting for user to enter OTP'''
+    print("waiting for user to enter otp")
+    #element=WebDriverWait(driver,30)
+    #element.until(EC.presence_of_element_located((By.XPATH,otpCont)))
+    #winddriver.implicitly_wait(30)
+    time.sleep(15)
+    continueOtp=driver.find_element_by_xpath(otpCont)
+    continueOtp.click()
+
+    ''' [NEEDS WORK] Clicking allow location and disable notification button '''
+    #driver.implicitly_wait(10)
+    time.sleep(3)
+    allowLocation = driver.find_element_by_xpath(allowLocationx)
+    allowLocation.click()
+    #driver.implicitly_wait(10)
+    time.sleep(2)
+    disableNotifications=driver.find_element_by_xpath(disableNotificationsx)
+    disableNotifications.click()
+    print("login succesful")
+
+login()
